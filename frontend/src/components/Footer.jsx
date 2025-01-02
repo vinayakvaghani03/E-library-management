4da7093f873a2400ce6a0a50952e3b4040ba8a1c
vinayakvaghani03 <vinayakwork8@gmail.com>
2025-01-02 21:02:21 +0530

    footer-data

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Footer = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center p-3 text-white bg-dark'style={{borderTop:"1px solid white"}}>
+      <p>© 2025 Condé Nast. All rights reserved. The New Yorker may earn a portion of sales from products that are purchased through our site as part of our Affiliate Partnerships with retailers. The material on this site may not be reproduced, distributed, transmitted, cached or otherwise used, except with the prior written permission of Condé Nast. Ad Choices</p>
+    </div>
+  );
+}
+
+export default Footer;
